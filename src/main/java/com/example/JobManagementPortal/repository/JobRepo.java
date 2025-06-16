e4cf153bfe457e34d157765445429a4aa9a11584
Vivek <inbox.vivekchauhan@gmail.com>
2025-06-16 19:48:51 +0530

    end moment commit

---
+++ src/main/java/com/example/JobManagementPortal/repository/JobRepo.java
@@ -0,0 +1,8 @@
+package com.example.JobManagementPortal.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class JobRepo {
+
+}
---
+++ src/main/java/com/example/JobManagementPortal/service/JobService.java
@@ -0,0 +1,20 @@
+package com.example.JobManagementPortal.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.JobManagementPortal.model.JobPost;
+
+@Service
+public class JobService {
+	
+	public void addJob() {
+		
+	}
+	
+	public List<JobPost> getAllJobs(){
+		
+	}
+
+}
